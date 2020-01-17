02f669e6d748d9e3e9959886abdb288048ceef1c
ganb <ganbinganxinhai@126.com>
2020-01-17 18:40:56 +0800

    plot_boxplot.r upgrade, support multiple data

--- plot_boxplot.r
+++ plot_boxplot.r
@@ -1,16 +1,18 @@
 #!/home/genesky/software/r/3.5.1/bin/Rscript
  
 library(docopt)
-"Usage: boxplot.r  -i <file> --pdf <file> [--Rlib <dir> --width <int>]
+"Usage: boxplot.r  -i <file> --pdf <file> [--Rlib <dir> --width <int> --y_name <string> ]
 Options:
-   -i, --input <file>    输入文件，两列，包含表头，第一列分组，第二列数值型数据
+   -i, --input <file>    输入文件，至少三列，包含表头，第一列样本名，第二列分组，第三列及之后的列为数值型数据，每一列对应一张图
    --pdf <file>          输出pdf文件路径
+   --y_name <string>     y轴名称
    --width <int>         pdf绘图时，每一个分组的宽度 [default: 2.5]
    --Rlib <dir>          R包路径  [default: /home/genesky/software/r/3.5.1/lib64/R/library] " -> doc
 
 opts                     <- docopt(doc, version = '点图绘制 \n          甘斌 129\n')
 input                    <- opts$input
 output_pdf               <- opts$pdf
+y_name                   <- opts$y_name
 width                    <- opts$width
 Rlib                     <- opts$Rlib
  
@@ -21,10 +23,13 @@ library(ggpubr, quietly = TRUE)
 
 # 读入数据
 data <- read.table(input, sep='\t', header = TRUE, check.names=FALSE) # 读取数据，行为样本
-col_names <- colnames(data)
+colnames(data)[c(1,2)] = c('SampleName', 'SampleGroup')
 
 # 分组数量
-group_count = length(unique(data[, 1]))
+group_count = length(unique(data[, 'SampleGroup']))
+
+# 基因数量
+genes = colnames(data)[3:ncol(data)]
 
 
 # 颜色模版
@@ -33,17 +38,25 @@ mycol <- colors()[rep(mycol, 50)]
 
 # pdf
 pdf(output_pdf, width = group_count * as.numeric(width), height = 7) # 一组2.5宽度
-        
-p <- ggboxplot(data, x=col_names[1], y=col_names[2], color = col_names[1], 
+
+for(gene in genes)
+{
+   data_plot = data[, c('SampleName', 'SampleGroup', gene)]
+   colnames(data_plot)[3] = 'GENE'  # 变量不能以数字开头，故替换名称
+   data_plot = data_plot[complete.cases(data_plot), ]  # 去掉缺失
+
+
+   message("plot : ", gene)
+   p <- ggboxplot(data_plot, x='SampleGroup', y='GENE', color = 'SampleGroup', 
           palette = "npg", #杂志nature的配色
           # palette = "aaas", #杂志Science的配色
           # palette = "jco", #按jco杂志配色方案
           add = c("jitter"),  # 增加点
-          add.params = list(color = col_names[1]),
+          add.params = list(color = 'SampleGroup'),
           outlier.size = -1, # 隐藏离群点，否则离群点会在图上出现两次（boxplot绘制离群点，jitter再绘制所有点，从而使离群点绘制两次）
-          title = 'boxplot'
-          )  + ylab(col_names[2]) + guides(fill = FALSE, color = FALSE) + theme(plot.title = element_text(hjust = 0.5))  # 去掉legend,标题居中  
-print(p)
-
+          title = gene
+          )  + ylab(y_name) + guides(fill = FALSE, color = FALSE) + theme(plot.title = element_text(hjust = 0.5))  # 去掉legend,标题居中  
+   print(p)
+}
 dev.off()
 
