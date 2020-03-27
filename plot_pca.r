020aabcde700fa4f4656b27294086b1e7be6c9c3
ganb <ganbinganxinhai@126.com>
2020-03-27 17:17:45 +0800

    update plot_pca.r with new feature: could set sample shape

--- plot_pca.r
+++ plot_pca.r
@@ -1,18 +1,20 @@
-#!/usr/bin/env Rscript
+#!/home/genesky/software/r/3.5.1/bin/Rscript
+
 .libPaths("/home/genesky/software/r/3.5.1/lib64/R/library/")
 library(docopt)
-"Usage: pca_plot.r [options] 
+"Usage: pca_plot.r --pca_file <file> --group_file <file> --output <pdf>  [--size <int> ] 
 Options:
-   --pca_file <file>           pca file with 3 column:sample pc1 pc2. must with head
-   --group_file <file>         group file with 2 column:sample group. must with head
+   --pca_file <file>           PCA坐标文件，第一列样本名，第二、三列分别是pc1/pc2坐标。
+   --group_file <file>         分组文件，含有表头。最多三列，可以只放入两列。第一列样本名，第二列分组1，图中用颜色区分。第三列分组2，图中用形状区分。
+   --size <int>                点的大小 [default: 1]
    --output <pdf>              pdf output file" -> doc
 
 opts                     <- docopt(doc, version = 'Program : plot pca v1.0 \n          甘斌 129\n')
 pca_file                 <- opts$pca_file
 group_file               <- opts$group_file
 output                   <- opts$output
-
-
+size                     <- as.numeric(opts$size)
+ 
 # # 测试用参数
 # pca_file                 <- '/home/ganb/work/tmp/17B1116A_18B0511B/18B0511B/output/sample_pca.txt'
 # group_file               <- '/home/ganb/work/tmp/17B1116A_18B0511B/18B0511B/group.txt'
@@ -22,23 +24,32 @@ library(ggplot2) # 正态分布检测要用
 
 pca_data   = read.table(pca_file, head = T, check.names = F, stringsAsFactors=F)
 group_data = read.table(group_file, head = T, check.names = F, stringsAsFactors=F)
+group_col_names = colnames(group_data)
 colnames(pca_data)[1:3] = c('Sample', 'PC1', 'PC2')
 colnames(group_data)[1:2] = c('Sample', 'Group')
+if(ncol(group_data) == 3) colnames(group_data)[3] = 'Group2'
 
 
 # 取出分组名，没有的话，定义为NO_GROUP
-group_name = unlist(lapply(pca_data$Sample, function(sample_name){ 
-        group = 'NO_GROUP'
-        if(sample_name %in% group_data$Sample) group = group_data[which(group_data$Sample == sample_name), 'Group']
+group_name = sapply(pca_data$Sample, function(sample_name){ 
+        group = c(sample_name, rep('NO_GROUP', (ncol(group_data) - 1)  ))
+        if(sample_name %in% group_data$Sample) group = group_data[which(group_data$Sample == sample_name), , drop=FALSE]
         group  
-    } ) )
-pca_data$Group = factor(group_name)
+    } ) 
+pca_data$Group = factor(unlist(group_name['Group', ]))
+if(ncol(group_data) == 3) pca_data$Group2 = factor(unlist(group_name['Group2', ]))
 
 # 颜色模版
 mycol <- c(119,132,147,454,89,404,123,463,461,128,139,552,28,54,100,258,558,376,43,652,165,31,610,477,256,588,99,632,81,503,104,562,76,96,495,598,645,507,657,33,179,107,62)  
 mycol <- colors()[rep(mycol, 50)] 
 
+# 形状模版
+myshape <- c(15, 16, 17, 18, 0:14)
+
 pdf(output)
-p <- ggplot(data = pca_data, aes(x = PC1, y = PC2, color = Group)) + geom_point() + scale_color_manual(values = mycol[1:length(levels(pca_data$Group))])
+
+if(ncol(group_data) == 2) p <- ggplot(data = pca_data, aes(x = PC1, y = PC2, color = Group)) + geom_point(size = size) + scale_color_manual(values = mycol[1:length(levels(pca_data$Group))]) + guides(color = guide_legend(title = group_col_names[2]))
+if(ncol(group_data) == 3) p <- ggplot(data = pca_data, aes(x = PC1, y = PC2, color = Group, shape = Group2)) + geom_point(size = size)  + scale_color_manual(values = mycol[1:length(levels(pca_data$Group))]) + scale_shape_manual(values=myshape[1:length(levels(pca_data$Group2))])  + guides(color = guide_legend(title = group_col_names[2]), shape = guide_legend(title = group_col_names[3]))
+
 p
 dev.off()
